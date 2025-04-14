e228425c4ea642260d690715b8b0a966c3bd9097
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2025-04-14 12:17:32 +0530

    Update header.php

--- wp/wp-content/themes/clearer/header.php
+++ wp/wp-content/themes/clearer/header.php
@@ -9,7 +9,7 @@
 	<link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
 	<?php endif; ?>
 <!-- Add manually above wp_head() -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer">
 	<?php wp_head(); ?>
 </head>
 
